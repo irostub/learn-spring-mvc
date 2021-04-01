6404e3b3b751346b9587f5b955297b6465445400
irostub <irostub@gmail.com>
2021-04-01 21:48:26 +0900

    Add two argument constructor

--- servlet-server/src/main/java/com/irostub/servletserver/domain/member/Member.java
+++ servlet-server/src/main/java/com/irostub/servletserver/domain/member/Member.java
@@ -5,9 +5,13 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Member {
     private Long id;
     private String username;
     private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 }
