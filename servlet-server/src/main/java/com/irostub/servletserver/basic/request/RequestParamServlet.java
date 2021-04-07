d5e03263b4a7beea612959a4070ea0906c67a41b
irostub <irostub@gmail.com>
2021-04-07 21:31:06 +0900

    Add request parameter servlet

---
+++ servlet-server/src/main/java/com/irostub/servletserver/basic/request/RequestParamServlet.java
@@ -0,0 +1,34 @@
+package com.irostub.servletserver.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회] - start");
+        req.getParameterNames().asIterator().forEachRemaining(paramName-> System.out.println(paramName + "=" + req.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+
+        System.out.println("[단일 파라미터 조회] - start");
+        String username = req.getParameter("username");
+        String age = req.getParameter("age");
+        System.out.println("[단일 파라미터 조회] - end");
+        System.out.println("username = "+username+", age = " + age);
+
+        System.out.println("[이름 중복 파라미터 조회] - start");
+        String[] usernames = req.getParameterValues("username");
+        for (String s : usernames) {
+            System.out.println("s = " + s);
+        }
+        System.out.println("[이름 중복 파라미터 조회] - end");
+
+        resp.getWriter().write("ok");
+    }
+}
