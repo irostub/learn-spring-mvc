3a777c8e4f43be9b5448732e8622284b0c130296
irostub <irostub@gmail.com>
2021-04-21 23:09:37 +0900

    Add request param type search controller

---
+++ spring-mvc/src/main/java/com/irostub/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,24 @@
+package com.irostub.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @GetMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        log.info("username = {}, age = {}", username, age);
+
+        resp.getWriter().write("ok");
+    }
+}
