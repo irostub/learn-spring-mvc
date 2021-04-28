7d45e997f9fb231bef2984becb7a90c9cc767809
irostub <irostub@gmail.com>
2021-04-28 22:04:41 +0900

    Add item domain
    add welcome page

---
+++ item-service/src/main/java/com/irostub/itemservice/domain/item/Item.java
@@ -0,0 +1,15 @@
+package com.irostub.itemservice.domain.item;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+}
