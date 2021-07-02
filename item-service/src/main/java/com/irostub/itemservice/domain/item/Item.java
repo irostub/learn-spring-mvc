5bf2209ca26f601265270b4fed91452423e11d40
irostub <irostub@gmail.com>
2021-07-02 20:59:04 +0900

    Remove @Builder in Item.java

--- item-service/src/main/java/com/irostub/itemservice/domain/item/Item.java
+++ item-service/src/main/java/com/irostub/itemservice/domain/item/Item.java
@@ -6,10 +6,15 @@ import lombok.*;
 @AllArgsConstructor
 @Getter
 @Setter
-@Builder
 public class Item {
     private Long id;
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
